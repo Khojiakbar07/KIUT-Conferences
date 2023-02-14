17cbbe551a1447eedc28974655baace814d81697
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-02-14 20:02:14 +0500

    Added partners and deans image

---
+++ src/components/create-event-link/CreateEventLink.jsx
@@ -0,0 +1,12 @@
+import "./CreateEventLink.scss";
+const CreateEventLink = () => {
+  return (
+    <section className="create-event">
+      <div className="container">
+        
+      </div>
+    </section>
+  );
+};
+
+export default CreateEventLink;
--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,9 +1,11 @@
 // components
+import CreateEventLink from "../../components/create-event-link/CreateEventLink";
 import Intro from "../../components/intro/Intro";
 const Home = () => {
   return (
     <main>
       <Intro />
+      <CreateEventLink/>
     </main>
   );
 };
