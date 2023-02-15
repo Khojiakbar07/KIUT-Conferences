a1dba21cbe2f0ebbcf47966ad8b6e13ee3e7af3b
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-02-15 12:52:54 +0500

    Speakers section was finished

--- src/components/speakers/Speakers.jsx
+++ src/components/speakers/Speakers.jsx
@@ -1,11 +1,42 @@
 import "./Speakers.scss";
 
+//images
+import speaker1 from "../../assets/speakers/speaker1.jpg";
+import speaker2 from "../../assets/speakers/speaker2.jpg";
+import speaker3 from "../../assets/speakers/speaker3.jpg";
+import speaker4 from "../../assets/speakers/speaker4.jpg";
+
 const Speakers = () => {
   return (
     <section className="speakers">
       <h3 className="speakers-title">Speakers</h3>
       <div className="container">
-        <div className="speakers-content"></div>
+        <div className="speakers-content">
+          <div className="speakers-box">
+            <div className="speakers-img">
+              <img src={speaker1} alt="Robert Kiyosaki" />
+            </div>
+            <p className="speakers-name">Robert Kiyosaki</p>
+          </div>
+          <div className="speakers-box">
+            <div className="speakers-img">
+              <img src={speaker2} alt="Robert Kiyosaki" />
+            </div>
+            <p className="speakers-name">Tony Robbins</p>
+          </div>
+          <div className="speakers-box">
+            <div className="speakers-img">
+              <img src={speaker3} alt="Robert Kiyosaki" />
+            </div>
+            <p className="speakers-name">Bill Gates</p>
+          </div>
+          <div className="speakers-box">
+            <div className="speakers-img">
+              <img src={speaker4} alt="Robert Kiyosaki" />
+            </div>
+            <p className="speakers-name">Batyrkhan Beysembekov</p>
+          </div>
+        </div>
       </div>
     </section>
   );
