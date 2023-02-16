c662a2a71c6d84d790c93b0ed02d399b6187f1df
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-02-16 14:20:58 +0500

    Conferences section started

---
+++ src/routes/conference/Conference.jsx
@@ -0,0 +1,11 @@
+import Intro from "../../components/intro/Intro"
+
+const Conference = () => {
+  return (
+    <main>
+      <Intro title={"CONFERENCE"} />
+    </main>
+  )
+}
+
+export default Conference
