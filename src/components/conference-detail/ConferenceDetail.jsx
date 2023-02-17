194c55d30a7edda89f8bf52438bc3e676fcb6701
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-02-17 10:51:10 +0500

    Conference detail page was finished

--- src/components/conference-detail/ConferenceDetail.jsx
+++ src/components/conference-detail/ConferenceDetail.jsx
@@ -1,7 +1,8 @@
 //tools
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { formatterDate } from "../../hooks/formatterDate";
 
 //SCSS
 import "./ConferenceDetail.scss";
@@ -12,6 +13,7 @@ const BASE_URL = "https://conference.alltravel.uz/apps";
 const ConferenceDetail = () => {
   const { conferenceId } = useParams();
   const [conferenceItem, setConferenceItem] = useState([]);
+  const lang = localStorage.getItem("lang");
   useEffect(() => {
     (async () => {
       try {
@@ -29,8 +31,45 @@ const ConferenceDetail = () => {
   console.log(conferenceItem);
 
   return (
-    <section>
-      <div className="container"></div>
+    <section className="conference-info">
+      <div className="container">
+        <div className="conference-info__hero">
+          <h2 className="conference-info__name">
+            {conferenceItem?.[`name_${lang}`]}
+          </h2>
+          <div className="conference-info__img">
+            <img src={conferenceItem?.image} alt="Conference" />
+          </div>
+        </div>
+        <div className="conference-info__content">
+          <p className="conference-info__desc">
+            {conferenceItem?.[`description_${lang}`]}
+          </p>
+          <div className="conference-info__info">
+            <div className="conference-info__card">
+              <ul className="conference-info__card__list">
+                <li className="conference-info__card__list-item">
+                  <strong>Fee</strong>
+                  <p>{conferenceItem?.[`price_${lang}`]}</p>
+                </li>
+                <li className="conference-info__card__list-item">
+                  <strong>Date and Time</strong>
+                  <p>{`${formatterDate(conferenceItem?.date_starts_time).day}.${
+                    formatterDate(conferenceItem?.date_starts_time).month
+                  }.${
+                    formatterDate(conferenceItem?.date_starts_time).year
+                  }`}</p>
+                </li>
+                <li className="conference-info__card__list-item">
+                  <strong>Address</strong>
+                  <p>Usman Nasyr Street, 156, 100121 Tashkent</p>
+                </li>
+              </ul>
+            </div>
+            <Link className="conference-info__payment-link" to="/payment">Payment</Link>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
