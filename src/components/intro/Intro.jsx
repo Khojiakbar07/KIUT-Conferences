279876a0c23230cfaeaaf42f9fae45528cca100f
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-02-20 10:18:03 +0500

    Intro refactored

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,11 +1,17 @@
 // SCSS
 import "./Intro.scss";
 
-const Intro = ({title}) => {
+const Intro = () => {
   return (
     <section className="intro">
       <div className="container">
-        <h2 className="intro-title section-title">{title}</h2>
+        <div className="intro-title__wrap">
+          <h1 className="intro-title">
+            Tashkent International Pedagogical Forum – Education: A Look Into
+            The Future (TIPF 2023) will be held on April 26-27, 2023 at the
+            Kimyo International University in Tashkent (Uzbekistan)
+          </h1>
+        </div>
       </div>
     </section>
   );
