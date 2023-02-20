f57e0cbfdcb19b565f877c192376bb445047c252
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-02-20 17:04:42 +0500

    deleted recent conferences and fixed

--- src/components/conference/conference-card/ConferenceCard.jsx
+++ src/components/conference/conference-card/ConferenceCard.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 //icons
 import { HiOutlineArrowLongRight } from "react-icons/hi2";
 
+import demoImg from "../../../assets/images/demo.jpg";
+
 //SCSS
 import "./ConferenceCard.scss";
 
@@ -12,14 +14,24 @@ const ConferenceCard = ({ conference }) => {
   return (
     <div className="conference-card">
       <div className="conference-card__img">
-        <img src={conference?.image} alt="Conference" />
+        {/* {conference?.image} */}
+        <img src={demoImg} alt="Conference" />
       </div>
       <div className="conference-card__content">
         <h2 className="conference-card__title">
-          {conference?.[`name_${localStorage.getItem("lang")}`]}
+          {/* {conference?.[`name_${localStorage.getItem("lang")}`]} */}
+          International Pedagogical Forum – Education: A Look Into The Future
+          (TIPF 2023)
         </h2>
         <p className="conference-card__desc">
-          {conference?.[`description_${localStorage.getItem("lang")}`]}
+          {/* {conference?.[`description_${localStorage.getItem("lang")}`]} */}
+          The Organizational Committee is delighted to announce that the
+          Tashkent International Pedagogical Forum – Education: A Look Into The
+          Future (TIPF 2023) will be held on April 26-27, 2023 at the Kimyo
+          International University in Tashkent (Uzbekistan) as hybrid, i.e. both
+          in-person and virtually. Thus, the participants from all over the
+          world will have the opportunity to participate both physically and
+          online, depending on their preferences.
         </p>
         <Link
           className="conference-card__link"
--- src/routes/index.js
+++ src/routes/index.js
@@ -7,7 +7,6 @@ import Layout from "../components/layout/Layout";
 import Home from "./home/Home";
 import About from "./about/About";
 import Payment from "./payment/Payment";
-import Test from "./Test";
 import Conferences from "./conferences/Conferences";
 import Detail from "./detail/Detail";
 import NotFound from "./not-found/NotFound";
@@ -15,13 +14,47 @@ import NotFound from "./not-found/NotFound";
 const index = () => {
   return (
     <Routes>
-        <Route path="/" element={<Layout><Home /></Layout>} />
-        <Route path="/about" element={<Layout><About /></Layout>} />
-        <Route path="/conference" element={<Layout><Conferences /></Layout>} />
-        <Route path="/detail/:conferenceId" element={<Layout><Detail /></Layout>} />
-        <Route path="/payment/:conferenceId" element={<Layout><Payment /></Layout>} />
-        <Route path="/test" element={<Test />} />
-        <Route path="*" element={<NotFound />} />
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <Home />
+          </Layout>
+        }
+      />
+      <Route
+        path="/about"
+        element={
+          <Layout>
+            <About />
+          </Layout>
+        }
+      />
+      <Route
+        path="/conference"
+        element={
+          <Layout>
+            <Conferences />
+          </Layout>
+        }
+      />
+      <Route
+        path="/detail/:conferenceId"
+        element={
+          <Layout>
+            <Detail />
+          </Layout>
+        }
+      />
+      <Route
+        path="/payment/:conferenceId"
+        element={
+          <Layout>
+            <Payment />
+          </Layout>
+        }
+      />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
