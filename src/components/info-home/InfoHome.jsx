cd0bf373248f358062cc2f97ce0a0bf6e406ff14
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-02-21 15:24:06 +0500

    info-home translated

--- src/components/info-home/InfoHome.jsx
+++ src/components/info-home/InfoHome.jsx
@@ -12,19 +12,8 @@ const InfoHome = () => {
     <section className="home-info">
       <div className="container">
         <div className="home-info__content">
-          <h2 className="dean-name">Janpolat Kudaybergenov</h2>
-          <p className="home-info__text">
-            In accordance with the Decree of the President of Uzbekistan
-            Republic PP-60, January 28, 2022 “On the development strategy of New
-            Uzbekistan for 2022-2026”, Decree of the President of the Republic
-            of Uzbekistan dated 08.10.2019 No. PF-5847 "Concept for the
-            development of the higher education system of the Republic of
-            Uzbekistan until 2030" April 26, 2023, at the Kimyo International
-            University in Tashkent as part of the Pedagogical Forum "Education:
-            a look into the future" is planned to hold the International
-            Scientific and Practical Conference "Processes of Modernization in
-            Education: Problems and Solutions".
-          </p>
+          <h2 className="dean-name">{t("jam")}</h2>
+          <p className="home-info__text">{t("self")}</p>
         </div>
         <div className="dean-img__wrap">
           <div className="dean-img">
