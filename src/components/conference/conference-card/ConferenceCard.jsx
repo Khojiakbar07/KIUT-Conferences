03c0ad3f4280fbe53c4e17e23bc7cca07a96ebb1
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-02-21 15:17:41 +0500

    translated conference card

--- src/components/conference/conference-card/ConferenceCard.jsx
+++ src/components/conference/conference-card/ConferenceCard.jsx
@@ -1,5 +1,6 @@
 //tools
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 //icons
 import { HiOutlineArrowLongRight } from "react-icons/hi2";
@@ -9,32 +10,19 @@ import demoImg from "../../../assets/images/demo.jpg";
 //SCSS
 import "./ConferenceCard.scss";
 
-const ConferenceCard = ({ conference }) => {
-  console.log(conference);
+const ConferenceCard = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="conference-card">
       <div className="conference-card__img">
-        {/* {conference?.image} */}
         <img src={demoImg} alt="Conference" />
       </div>
       <div className="conference-card__content">
-        <h2 className="conference-card__title">
-          {/* {conference?.[`name_${localStorage.getItem("lang")}`]} */}
-          International Pedagogical Forum – Education: A Look Into The Future
-          (TIPF 2023)
-        </h2>
-        <p className="conference-card__desc">
-          {/* {conference?.[`description_${localStorage.getItem("lang")}`]} */}
-          The Organizational Committee is delighted to announce that the
-          Tashkent International Pedagogical Forum – Education: A Look Into The
-          Future (TIPF 2023) will be held on April 26-27, 2023 at the Kimyo
-          International University in Tashkent (Uzbekistan) as hybrid, i.e. both
-          in-person and virtually. Thus, the participants from all over the
-          world will have the opportunity to participate both physically and
-          online, depending on their preferences.
-        </p>
+        <h2 className="conference-card__title">{t("intr")}</h2>
+        <p className="conference-card__desc">{t("title_intr")}</p>
         <Link className="conference-card__link" to={`/detail`}>
-          <span>Read More</span> <HiOutlineArrowLongRight />
+          <span>{t("read")}</span> <HiOutlineArrowLongRight />
         </Link>
       </div>
     </div>
