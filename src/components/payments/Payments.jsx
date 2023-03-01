a3f7ee2d77fe72153d4bce03306a427775d40128
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-03-01 07:18:57 +0500

    removed junks

--- src/components/payments/Payments.jsx
+++ src/components/payments/Payments.jsx
@@ -4,15 +4,9 @@ import PaymentForm from "./payment-form/PaymentForm";
 //SCSS
 import "./Payments.scss";
 //images
-import QR from "../../assets/images/qr-code.png";
-import scanGuide from "../../assets/images/scan-me.png";
 import click from "../../assets/payment-types/click.png";
-import apelsin from "../../assets/payment-types/apelsin.png";
 import humo from "../../assets/payment-types/Humo.png";
-import payme from "../../assets/payment-types/payme.png";
 import uzcard from "../../assets/payment-types/Uzcard.png";
-import masterCard from "../../assets/payment-types/master-card.png";
-import visaCard from "../../assets/payment-types/visa-card.png";
 
 const Payments = () => {
   const { t } = useTranslation();
@@ -20,39 +14,7 @@ const Payments = () => {
     <section className="payments">
       <div className="container">
         <h3 className="payments-title line">{t("submission")}</h3>
-        <div className="payments__content">
-          <PaymentForm />
-          <div className="payments-QR">
-            <p>QR Code for pay</p>
-            <img src={QR} alt="" />
-          </div>
-        </div>
-        <div className="payments-guide">
-          <div className="payments__scan-me">
-            <img src={scanGuide} alt="You should scan QR code" />
-          </div>
-          <div className="payments-type">
-            <img className="click" src={click} alt="You can pay via click" />
-            <img className="payme" src={payme} alt="You can pay via payme" />
-            <img
-              className="apelsin"
-              src={apelsin}
-              alt="You can pay via apelsin"
-            />
-            <img
-              className="master-card"
-              src={masterCard}
-              alt="You can pay via master card"
-            />
-            <img className="humo" src={humo} alt="You can pay via humo" />
-            <img className="uzcard" src={uzcard} alt="You can pay via uzcard" />
-            <img
-              className="visa-card"
-              src={visaCard}
-              alt="You can pay via visa card"
-            />
-          </div>
-        </div>
+        <div className="payments__content"></div>
       </div>
     </section>
   );
