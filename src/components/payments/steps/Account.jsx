6cff410e1323e37217e3a169618a55d82fd50961
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-03-03 16:15:07 +0500

    added drop down logic

--- src/components/payments/steps/Account.jsx
+++ src/components/payments/steps/Account.jsx
@@ -1,21 +1,35 @@
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { v4 as uuidv4 } from "uuid";
 
 //react phone number
 import "react-phone-number-input/style.css";
 import PhoneInput from "react-phone-number-input";
 
+//icon
+import { FiChevronDown } from "react-icons/fi";
+
 //SCSS
 import "./Account.scss";
 
+import dropdownData from "../../../dummy-data/dropdown-data.json";
+
 export default function Account({ handleClick }) {
   const { t } = useTranslation();
-  const [fullName, setFullName] = useState(localStorage.getItem("fullname"));
-  const [workedPlace, setWorkedPlace] = useState(
-    localStorage.getItem("worked_place")
+  const [fullName, setFullName] = useState(
+    localStorage.getItem("fullname") || ""
   );
-  const [phoneNumber, setPhoneNumber] = useState(localStorage.getItem("phone"));
-  const [email, setEmail] = useState(localStorage.getItem("email"));
+  const [workedPlace, setWorkedPlace] = useState(
+    localStorage.getItem("worked_place") || ""
+  );
+  const [phoneNumber, setPhoneNumber] = useState(
+    localStorage.getItem("phone") || ""
+  );
+  const [email, setEmail] = useState(localStorage.getItem("email") || "");
+
+  // dropdown
+  const [isDropdownActive, setDropdownActive] = useState(false);
+  const [selected, setSelected] = useState("Select...");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -58,35 +72,70 @@ export default function Account({ handleClick }) {
           />
         </div>
       </div>
-      <div className="payment-form__input phone-num">
-        <label htmlFor="phoneNumber">{t("phoneNumber")}</label>
-        <PhoneInput
-          id="phoneNumber"
-          placeholder={t("phoneNumber")}
-          value={phoneNumber}
-          onChange={setPhoneNumber}
-          required
-        />
+      <div className="account-form__inputs">
+        <div className="payment-form__input phone-num flex">
+          <label htmlFor="phoneNumber">{t("phoneNumber")}</label>
+          <PhoneInput
+            id="phoneNumber"
+            placeholder={t("phoneNumber")}
+            value={phoneNumber}
+            onChange={setPhoneNumber}
+            required
+          />
+        </div>
+        <div className="payment-form__input email-input flex">
+          <label htmlFor="email">{t("email")}</label>
+          <input
+            type="email"
+            placeholder={t("email")}
+            value={email}
+            id="email"
+            required
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </div>
       </div>
-      <div className="payment-form__input email-input">
-        <label htmlFor="email">{t("email")}</label>
-        <input
-          type="email"
-          placeholder={t("email")}
-          value={email}
-          id="email"
-          required
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
+      <div className="account-form__select payment-form__inputs">
+        <label htmlFor="dropdown">Conference section</label>
+        <div
+          className={`dropdown flex ${isDropdownActive ? "active" : ""}`}
+          id="dropdown"
+        >
+          <div
+            className="dropdown-btn"
+            onClick={() => {
+              setDropdownActive(!isDropdownActive);
+            }}
+          >
+            <span>{selected}</span>{" "}
+            <span className="down-icon">
+              <FiChevronDown />
+            </span>
+          </div>
+          <ul className="dropdown-content">
+            {dropdownData?.[0]?.[localStorage.getItem("lang")].map(
+              ({ data }, idx) => (
+                <li
+                  onClick={(e) => {
+                    setSelected(e.target.textContent);
+                    setDropdownActive(false);
+                  }}
+                  key={uuidv4()}
+                  className="dropdown-content__item"
+                >
+                  <span className="dropdown-content__text">
+                    {idx + 1}. {data}
+                  </span>
+                </li>
+              )
+            )}
+          </ul>
+        </div>
       </div>
-      <button
-        // onClick={() => handleClick("next")}
-        className="account-form__submit-btn"
-      >
-        Next
-      </button>
+
+      <button className="account-form__submit-btn">Next</button>
     </form>
   );
 }
