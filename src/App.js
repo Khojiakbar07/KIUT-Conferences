c7c8f9735679e1b4acd98cf3a9adca89b3059890
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-03-05 19:18:22 +0500

    alert iu ready

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Layout from "./components/layout/Layout";
 import { BackToTop, Loader } from "./utils";
 import { LoaderContext } from "./context/LoaderContext";
 import { useState } from "react";
+import Alert from "./components/alert/Alert";
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
   return (
     <div className="App">
+      <Alert />
       <Layout>
         <LoaderContext.Provider value={{ setIsLoading }}>
           <Outlet />
--- src/components/alert/Alert.jsx
+++ src/components/alert/Alert.jsx
@@ -1,7 +1,17 @@
+//icons
+import errorIcon from "../../assets/ui-icons/error.svg";
+import successIcon from "../../assets/ui-icons/success.svg";
+
+// SCSS
 import "./Alert.scss";
 
 export default function Alert() {
-  return <div className="alert">
-    
-  </div>;
+  return (
+    <div className="alert">
+      <h5 className="alert-title line">SUCCSESSFULly registrated!</h5>
+      <img className="alert-icon" src={successIcon} alt="" />
+      <strong className="alert-message">Success</strong>
+      <button className="alert-btn">OK</button>
+    </div>
+  );
 }
