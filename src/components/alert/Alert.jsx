207246476728abb5a8d319418c8109115a4cd48b
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-03-13 15:26:02 +0500

    fix

--- src/components/alert/Alert.jsx
+++ src/components/alert/Alert.jsx
@@ -1,6 +1,6 @@
-import {useEffect, useState} from "react";
-import {useTranslation} from "react-i18next";
-import {useSearchParams} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useSearchParams } from "react-router-dom";
 //icons
 import errorIcon from "../../assets/ui-icons/error.svg";
 import successIcon from "../../assets/ui-icons/success.svg";
@@ -8,49 +8,48 @@ import successIcon from "../../assets/ui-icons/success.svg";
 // SCSS
 import "./Alert.scss";
 
-export default function Alert({paymentStatus, setPaymentStatus}) {
-    const {t} = useTranslation();
-    const [resMessage, setResMessage] = useState("");
-    const [searchParams, setSearchParams] = useSearchParams();
-    useEffect(() => {
-        if (paymentStatus === 201) {
-            setResMessage(t("success_message"));
-        } else {
-            setResMessage(t("error_message"));
-        }
-    }, [paymentStatus, t]);
-    console.log(paymentStatus);
+export default function Alert({ paymentStatus, setPaymentStatus }) {
+  const { t } = useTranslation();
+  const [resMessage, setResMessage] = useState("");
+  const [searchParams, setSearchParams] = useSearchParams();
+  useEffect(() => {
+    if (paymentStatus === 201) {
+      setResMessage(t("success_message"));
+    } else {
+      setResMessage(t("error_message"));
+    }
+  }, [paymentStatus, t]);
 
-    const handleClick = () => {
-        if (paymentStatus === 201) {
-            localStorage.clear();
-            searchParams.delete('payment_status')
-            searchParams.delete('payment_id')
-            setSearchParams(searchParams);
-            window.location.reload();
-        }
-        setPaymentStatus(null);
-    };
+  const handleClick = () => {
+    if (paymentStatus === 201) {
+      localStorage.clear();
+      searchParams.delete("payment_status");
+      searchParams.delete("payment_id");
+      setSearchParams(searchParams);
+      window.location.reload();
+    }
+    setPaymentStatus(null);
+  };
 
-    return (
-        <div className={`alert ${paymentStatus ? "active" : ""}`}>
-            <h5 className="alert-title line">{resMessage}</h5>
-            <img
-                className="alert-icon"
-                src={paymentStatus === 201 ? successIcon : errorIcon}
-                alt=""
-            />
-            <strong
-                style={
-                    paymentStatus === 201 ? {color: "#27ae60"} : {color: "#f21e1e"}
-                }
-                className="alert-message"
-            >
-                {paymentStatus === 201 ? "Success" : `${t("error")}`}
-            </strong>
-            <button onClick={handleClick} className="alert-btn">
-                {paymentStatus === 201 ? "OK" : `${t("try_again")}`}
-            </button>
-        </div>
-    );
+  return (
+    <div className={`alert ${paymentStatus ? "active" : ""}`}>
+      <h5 className="alert-title line">{resMessage}</h5>
+      <img
+        className="alert-icon"
+        src={paymentStatus === 201 ? successIcon : errorIcon}
+        alt=""
+      />
+      <strong
+        style={
+          paymentStatus === 201 ? { color: "#27ae60" } : { color: "#f21e1e" }
+        }
+        className="alert-message"
+      >
+        {paymentStatus === 201 ? "Success" : `${t("error")}`}
+      </strong>
+      <button onClick={handleClick} className="alert-btn">
+        {paymentStatus === 201 ? "OK" : `${t("try_again")}`}
+      </button>
+    </div>
+  );
 }
