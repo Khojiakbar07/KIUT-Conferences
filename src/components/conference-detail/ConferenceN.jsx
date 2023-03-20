7f11151c10f8dc6594a30b716785fbf9e12b5bdf
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-03-20 14:50:33 +0500

    added namangan conference detail

--- src/components/conference-detail/ConferenceN.jsx
+++ src/components/conference-detail/ConferenceN.jsx
@@ -3,8 +3,12 @@ import { useTranslation } from "react-i18next";
 //image
 import conferenceImg from "../../assets/images/conference2.jpg";
 
+// data
+import confData from "../../dummy-data/namangan-detail.json";
+
 export default function ConferenceN() {
   const { t } = useTranslation();
+  const lang = localStorage.getItem("lang");
   return (
     <section className="conference-info">
       <div className="container">
@@ -20,7 +24,7 @@ export default function ConferenceN() {
             <ul className="conference-info__card__list">
               <li className="conference-info__card__list-item">
                 <strong>{t("online")}</strong>
-                <p>{t("september")} 1. 2022</p>
+                <p>{t("may")} 25. 2023</p>
               </li>
               <li className="conference-info__card__list-item">
                 <strong>{t("online_regis")}</strong>
@@ -28,17 +32,41 @@ export default function ConferenceN() {
               </li>
               <li className="conference-info__card__list-item">
                 <strong>{t("conferen")}</strong>
-                <p>{t("april")} 26-27, 2023</p>
+                <p>{t("may")} 25-26, 2023</p>
               </li>
             </ul>
           </div>
         </div>
         <div className="conference-info__content">
-          <p className="conference-info__text">{t("organiz")}</p>
-          <p className="conference-info__text">{t("part")}</p>
-          <strong className="conference-info__text track-title">
-            {t("thms")}
+          <p className="conference-info__text">{confData[0][lang].detail}</p>
+          <div className="conference-info__text">
+            {confData[0][lang].list.title}
+          </div>
+          <ol style={{ marginBottom: "40px" }}>
+            {confData[0][lang].list.priorities.map((p, index) => (
+              <li className="conference-info__text">
+                {index + 1}. {p}
+              </li>
+            ))}
+          </ol>
+
+          <strong
+            style={{ marginBottom: "0", display: "block" }}
+            className="conference-info__text track-title"
+          >
+            {t("zoom_meeting")}
           </strong>
+          <a
+            style={{ textDecoration: "underline" }}
+            href={confData[0][lang].link}
+            className="conference-info__text"
+          >
+            {confData[0][lang].link}
+          </a>
+          <p style={{ marginBottom: "0" }} className="conference-info__text">
+            {t("zoom_id")} {confData[0][lang].id} {t("access_code")}{" "}
+            {confData[0][lang].access_code}.
+          </p>
         </div>
       </div>
     </section>
