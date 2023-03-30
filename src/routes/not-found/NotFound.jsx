8531163cde14b1350624eba40840056974d7098b
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-03-30 14:40:37 +0500

    styled not found page

--- src/routes/not-found/NotFound.jsx
+++ src/routes/not-found/NotFound.jsx
@@ -1,8 +1,16 @@
-
 const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
-}
+    return (
+        <div style={{ height: "100vh", display: "grid", placeItems: "center" }}>
+            <div style={{ textAlign: "center" }}>
+                <span style={{ fontSize: "150px", fontWeight: "bold" }}>
+                    404
+                </span>
+                <p style={{ fontSize: "36px", fontWeight: "bold" }}>
+                    Page Not Found!
+                </p>
+            </div>
+        </div>
+    );
+};
 
-export default NotFound
+export default NotFound;
