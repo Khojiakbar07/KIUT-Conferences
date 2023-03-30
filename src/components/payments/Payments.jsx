fa9e6737ab495347c756148239eeab38214f3f95
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-03-30 14:28:01 +0500

    added drop down logic

--- src/components/payments/Payments.jsx
+++ src/components/payments/Payments.jsx
@@ -15,77 +15,77 @@ import Alert from "../alert/Alert";
 //SCSS
 import "./Payments.scss";
 
-const Payments = ({title}) => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  if (
-    searchParams.get("payment_status") === "2" &&
-    searchParams.get("payment_id")
-  ) {
-    localStorage.setItem("payed", "1");
-    localStorage.setItem("step", "3");
-    localStorage.setItem("payment_id", searchParams.get("payment_id"));
-  }
-
-  const { t } = useTranslation();
-  const [currentStep, setCurrentStep] = useState(
-    localStorage.getItem("step") ? localStorage.getItem("step") : 1
-  );
-  const steps = ["Step 1", "Step 2", "Step 3"];
-
-  const [paymentStatus, setPaymentStatus] = useState(null);
-
-  const displayStep = (step) => {
-    switch (parseInt(step)) {
-      case 1:
-        return <Account handleClick={handleClick} />;
-      case 2:
-        return <Payment handleClick={handleClick} />;
-      case 3:
-        return <UploadFile handleClick={handleClick} />;
-
-      default:
+const Payments = ({ title }) => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    if (
+        searchParams.get("payment_status") === "2" &&
+        searchParams.get("payment_id")
+    ) {
+        localStorage.setItem("payed", "1");
+        localStorage.setItem("step", "3");
+        localStorage.setItem("payment_id", searchParams.get("payment_id"));
     }
-  };
 
-  const handleClick = (direction) => {
-    let newStep = currentStep;
+    const { t } = useTranslation();
+    const [currentStep, setCurrentStep] = useState(
+        localStorage.getItem("step") ? localStorage.getItem("step") : 1
+    );
+    const steps = ["Step 1", "Step 2", "Step 3"];
 
-    // check if steps are within bounds
-    if (localStorage.getItem('isNamangan') === '1') {
-      direction === "next" ? newStep += 2 : newStep -= 2;
+    const [paymentStatus, setPaymentStatus] = useState(null);
 
-      localStorage.setItem("step", newStep);
-      newStep > 0 && newStep <= steps.length && setCurrentStep(newStep);
-    } else {
-      direction === "next" ? newStep++ : newStep--;
+    const displayStep = (step) => {
+        switch (parseInt(step)) {
+            case 1:
+                return <Account handleClick={handleClick} />;
+            case 2:
+                return <Payment handleClick={handleClick} />;
+            case 3:
+                return <UploadFile handleClick={handleClick} />;
 
-      localStorage.setItem("step", newStep);
-      newStep > 0 && newStep <= steps.length && setCurrentStep(newStep);
-    }
-  };
+            default:
+        }
+    };
 
-  return (
-    <section className="payments">
-      <AlertContext.Provider value={{ setPaymentStatus }}>
-        <Alert
-          paymentStatus={paymentStatus}
-          setPaymentStatus={setPaymentStatus}
-        />
-        <div className="container">
-          <h3 className="payments-title line">{t(title)}</h3>
-          <div className="payments__content">
-            <div className="progress-title">{t("regis")}</div>
-            <div className="stepper-wrapper">
-              {/* Stepper */}
-              <Stepper steps={steps} currentStep={currentStep} />
-              {/* Display Components */}
-              {displayStep(currentStep)}
-            </div>
-          </div>
-        </div>
-      </AlertContext.Provider>
-    </section>
-  );
+    const handleClick = (direction) => {
+        let newStep = currentStep;
+
+        // check if steps are within bounds
+        if (localStorage.getItem("isNamangan") === "1") {
+            direction === "next" ? (newStep += 2) : (newStep -= 2);
+
+            localStorage.setItem("step", newStep);
+            newStep > 0 && newStep <= steps.length && setCurrentStep(newStep);
+        } else {
+            direction === "next" ? newStep++ : newStep--;
+
+            localStorage.setItem("step", newStep);
+            newStep > 0 && newStep <= steps.length && setCurrentStep(newStep);
+        }
+    };
+
+    return (
+        <section className="payments">
+            <AlertContext.Provider value={{ setPaymentStatus }}>
+                <Alert
+                    paymentStatus={paymentStatus}
+                    setPaymentStatus={setPaymentStatus}
+                />
+                <div className="container">
+                    <h3 className="payments-title line">{t(title)}</h3>
+                    <div className="payments__content">
+                        <div className="progress-title">{t("regis")}</div>
+                        <div className="stepper-wrapper">
+                            {/* Stepper */}
+                            <Stepper steps={steps} currentStep={currentStep} />
+                            {/* Display Components */}
+                            {displayStep(currentStep)}
+                        </div>
+                    </div>
+                </div>
+            </AlertContext.Provider>
+        </section>
+    );
 };
 
 export default Payments;
--- src/components/payments/steps/Account.jsx
+++ src/components/payments/steps/Account.jsx
@@ -15,150 +15,156 @@ import "./Account.scss";
 
 // data
 import dropdownData from "../../../dummy-data/dropdown-data.json";
+import dropdownDataNamangan from "../../../dummy-data/dropdown-data-namangan.json";
 
 export default function Account({ handleClick }) {
-  const { pathname } = useLocation();
-  const { t } = useTranslation();
-  const [fullName, setFullName] = useState(
-    localStorage.getItem("fullname") || ""
-  );
-  const [workedPlace, setWorkedPlace] = useState(
-    localStorage.getItem("worked_place") || ""
-  );
-  const [phoneNumber, setPhoneNumber] = useState(
-    localStorage.getItem("phone") || ""
-  );
-  const [email, setEmail] = useState(localStorage.getItem("email") || "");
-  const [bill, setBill] = useState("");
+    const { pathname } = useLocation();
+    const { t } = useTranslation();
+    const [fullName, setFullName] = useState(
+        localStorage.getItem("fullname") || ""
+    );
+    const [workedPlace, setWorkedPlace] = useState(
+        localStorage.getItem("worked_place") || ""
+    );
+    const [phoneNumber, setPhoneNumber] = useState(
+        localStorage.getItem("phone") || ""
+    );
+    const [email, setEmail] = useState(localStorage.getItem("email") || "");
+    const [bill, setBill] = useState("");
 
-  // dropdown
-  const dropdownPlaceholder = t("select") + "...";
-  const [isDropdownActive, setDropdownActive] = useState(false);
-  const [selected, setSelected] = useState(dropdownPlaceholder);
+    // dropdown
+    const dropdownPlaceholder = t("select") + "...";
+    const [isDropdownActive, setDropdownActive] = useState(false);
+    const [selected, setSelected] = useState(dropdownPlaceholder);
 
-  const [isNamangan, setIsNamangan] = useState(pathname === "/submission/namangan");
+    const isNamangan = pathname === "/submission/namangan";
+    const renderData = isNamangan ? dropdownDataNamangan : dropdownData;
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    localStorage.setItem("accounted", true);
-    localStorage.setItem("fullname", fullName);
-    localStorage.setItem("worked_place", workedPlace);
-    localStorage.setItem("phone", phoneNumber);
-    localStorage.setItem("email", email);
-    localStorage.setItem("conference", selected);
-    localStorage.setItem("bill", bill);
-    localStorage.setItem("isNamangan", isNamangan);
-    selected !== dropdownPlaceholder ? handleClick("next") : <></>;
-  };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        localStorage.setItem("accounted", true);
+        localStorage.setItem("fullname", fullName);
+        localStorage.setItem("worked_place", workedPlace);
+        localStorage.setItem("phone", phoneNumber);
+        localStorage.setItem("email", email);
+        localStorage.setItem("conference", selected);
+        localStorage.setItem("bill", bill);
+        localStorage.setItem("isNamangan", isNamangan);
+        selected !== dropdownPlaceholder ? handleClick("next") : <></>;
+    };
 
-  return (
-    <form className="account-form" onSubmit={handleSubmit}>
-      <div className="account-form__inputs">
-        <div className="payment-form__input flex">
-          <label htmlFor="fullName">{t("fullname")}</label>
-          <input
-            type="text"
-            placeholder={t("fullname")}
-            value={fullName}
-            id="fullName"
-            required
-            onChange={(e) => {
-              setFullName(e.target.value);
-            }}
-          />
-        </div>
-        <div className="payment-form__input flex">
-          <label htmlFor="work-place">{t("work")}</label>
-          <input
-            type="text"
-            placeholder={t("work")}
-            value={workedPlace}
-            id="work-place"
-            onChange={(e) => {
-              setWorkedPlace(e.target.value);
-            }}
-            required
-          />
-        </div>
-      </div>
-      <div className="account-form__inputs">
-        <div className="payment-form__input phone-num flex">
-          <label htmlFor="phoneNumber">{t("phoneNumber")}</label>
-          <PhoneInput
-            id="phoneNumber"
-            placeholder={t("phoneNumber")}
-            value={phoneNumber}
-            onChange={setPhoneNumber}
-            required
-          />
-        </div>
-        <div className="payment-form__input email-input flex">
-          <label htmlFor="email">{t("email")}</label>
-          <input
-            type="email"
-            placeholder={t("email")}
-            value={email}
-            id="email"
-            required
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          />
-        </div>
-      </div>
-      <div className="account-form__select payment-form__input">
-        <label htmlFor="dropdown">{t("conference_section")}</label>
-        <div
-          className={`dropdown flex ${isDropdownActive ? "active" : ""}`}
-          id="dropdown"
-        >
-          <div
-            className="dropdown-btn"
-            onClick={() => {
-              setDropdownActive(!isDropdownActive);
-            }}
-          >
-            <span>{selected}</span>{" "}
-            <span className="down-icon">
-              <FiChevronDown />
-            </span>
-          </div>
-          <ul className="dropdown-content">
-            {dropdownData?.[0]?.[localStorage.getItem("lang")].map(
-              ({ data }, idx) => (
-                <li
-                  onClick={(e) => {
-                    setSelected(e.target.textContent);
-                    setDropdownActive(false);
-                  }}
-                  key={uuidv4()}
-                  className="dropdown-content__item"
+    return (
+        <form className="account-form" onSubmit={handleSubmit}>
+            <div className="account-form__inputs">
+                <div className="payment-form__input flex">
+                    <label htmlFor="fullName">{t("fullname")}</label>
+                    <input
+                        type="text"
+                        placeholder={t("fullname")}
+                        value={fullName}
+                        id="fullName"
+                        required
+                        onChange={(e) => {
+                            setFullName(e.target.value);
+                        }}
+                    />
+                </div>
+                <div className="payment-form__input flex">
+                    <label htmlFor="work-place">{t("work")}</label>
+                    <input
+                        type="text"
+                        placeholder={t("work")}
+                        value={workedPlace}
+                        id="work-place"
+                        onChange={(e) => {
+                            setWorkedPlace(e.target.value);
+                        }}
+                        required
+                    />
+                </div>
+            </div>
+            <div className="account-form__inputs">
+                <div className="payment-form__input phone-num flex">
+                    <label htmlFor="phoneNumber">{t("phoneNumber")}</label>
+                    <PhoneInput
+                        id="phoneNumber"
+                        placeholder={t("phoneNumber")}
+                        value={phoneNumber}
+                        onChange={setPhoneNumber}
+                        required
+                    />
+                </div>
+                <div className="payment-form__input email-input flex">
+                    <label htmlFor="email">{t("email")}</label>
+                    <input
+                        type="email"
+                        placeholder={t("email")}
+                        value={email}
+                        id="email"
+                        required
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                        }}
+                    />
+                </div>
+            </div>
+            <div className="account-form__select payment-form__input">
+                <label htmlFor="dropdown">{t("conference_section")}</label>
+                <div
+                    className={`dropdown flex ${
+                        isDropdownActive ? "active" : ""
+                    }`}
+                    id="dropdown"
                 >
-                  <span className="dropdown-content__text">
-                    {idx + 1}. {data}
-                  </span>
-                </li>
-              )
+                    <div
+                        className="dropdown-btn"
+                        onClick={() => {
+                            setDropdownActive(!isDropdownActive);
+                        }}
+                    >
+                        <span>{selected}</span>{" "}
+                        <span className="down-icon">
+                            <FiChevronDown />
+                        </span>
+                    </div>
+                    <ul className="dropdown-content">
+                        {renderData?.[0]?.[localStorage.getItem("lang")].map(
+                            ({ data }, idx) => (
+                                <li
+                                    onClick={(e) => {
+                                        setSelected(e.target.textContent);
+                                        setDropdownActive(false);
+                                    }}
+                                    key={uuidv4()}
+                                    className="dropdown-content__item"
+                                >
+                                    <span className="dropdown-content__text">
+                                        {idx + 1}. {data}
+                                    </span>
+                                </li>
+                            )
+                        )}
+                    </ul>
+                </div>
+            </div>
+            {!isNamangan ? (
+                <div className="payment-form__input bill flex">
+                    <label htmlFor="bill">{t("payment")}</label>
+                    <input
+                        type="number"
+                        placeholder={t("payment_amount")}
+                        value={bill}
+                        id="bill"
+                        required
+                        onChange={(e) => {
+                            setBill(e.target.value);
+                        }}
+                    />
+                </div>
+            ) : (
+                <></>
             )}
-          </ul>
-        </div>
-      </div>
-        {!isNamangan ? 
-          <div className="payment-form__input bill flex">
-            <label htmlFor="bill">{t("payment")}</label>
-            <input
-                type="number"
-                placeholder={t("payment_amount")}
-                value={bill}
-                id="bill"
-                required
-                onChange={(e) => {
-                  setBill(e.target.value);
-                }}
-            />
-          </div>
-    : <></>}
-      <button className="account-form__submit-btn">{t("next")}</button>
-    </form>
-  );
+            <button className="account-form__submit-btn">{t("next")}</button>
+        </form>
+    );
 }
