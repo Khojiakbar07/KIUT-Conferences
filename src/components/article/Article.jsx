e3e73a0a8fe2ff86f2afc4ced4350e528ad3f610
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-08-07 04:24:14 +0500

    publications was made responsive

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -3,40 +3,37 @@ import "./Article.scss";
 import ArticleItem from "./ArticleItem/ArticleItem";
 
 const Article = ({ sections, articles, title }) => {
-    const navigate = useNavigate();
-    const { pathname } = useLocation();
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
 
-    const handleSelectChange = (e) => {
-        navigate(`${pathname}?section=${e.target.value}`);
-    };
+  const handleSelectChange = (e) => {
+    navigate(`${pathname}?section=${e.target.value}`);
+  };
 
-    return (
-        <section className="article">
-            <div className="container">
-                <h2 className="article-title">{title}</h2>
-                <select
-                    onChange={handleSelectChange}
-                    className="article-select"
-                >
-                    {sections.map((section) => (
-                        <option key={section.id} value={section.id}>
-                            {section.name}
-                        </option>
-                    ))}
-                </select>
+  return (
+    <section className="article">
+      <div className="container">
+        <h2 className="article-title">{title}</h2>
+        <select onChange={handleSelectChange} className="article-select">
+          {sections.map((section) => (
+            <option key={section.id} value={section.id}>
+              {section.name}
+            </option>
+          ))}
+        </select>
 
-                <ol className="article__list">
-                    {articles.length ? (
-                        articles.map((article) => (
-                            <ArticleItem key={article.id} article={article} />
-                        ))
-                    ) : (
-                        <li className="article-item__name">No Articles</li>
-                    )}
-                </ol>
-            </div>
-        </section>
-    );
+        <ol className="article__list">
+          {articles.length ? (
+            articles.map((article) => (
+              <ArticleItem key={article.id} article={article} />
+            ))
+          ) : (
+            <li className="article-item__name">No Articles</li>
+          )}
+        </ol>
+      </div>
+    </section>
+  );
 };
 
 export default Article;
